4e324b08fd3eb6fba6188d0c3a1a720131a47daf
IThoseok <122499200+IThoseok@users.noreply.github.com>
2023-08-08 22:48:00 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 11.16 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -3,11 +3,7 @@ class Solution {
         String answer = "";
         String before = "";
         for(char x : s.toCharArray()){
-            if(x>48 && x<57){//숫자패스
-                before = Character.toString(x);
-                answer += Character.toString(x);
-                continue;
-            }
+
             if(before.length() == 0 || before.equals(" ")){//공백다음 or 첫글자
                 answer += Character.toUpperCase(x);
                 before = Character.toString(x);
