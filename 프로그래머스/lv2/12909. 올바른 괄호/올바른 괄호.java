3465943972f8bb58d7b0a64fa514653d6932bbd8
IThoseok <122499200+IThoseok@users.noreply.github.com>
2023-08-10 00:15:08 +0900

    [level 2] Title: 올바른 괄호, Time: 11.89 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,35 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int length = 0;
+        
+        if(s.equals("")){
+            return false;
+        }
+        
+        s = s.replace("()", "");
+        if(s.equals("")){
+            return true;
+        }
+        
+        if(s.length()%2==1 ){ 
+            return false;
+        }
+        
+        if(s.indexOf("(",s.length()-1) != -1){
+            return false;
+        }
+        
+        if(s.indexOf("(",0) == -1){
+            return false;
+        }
+        
+        length = s.length();
+        if(length != s.replace(String.valueOf(')'),"").length()*2){
+            return false;
+        }
+
+
+        return answer;
+    }
+}
