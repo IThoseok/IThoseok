99dcddb813f77c0b27b399bfcd771865fa1c48a9
IThoseok <122499200+IThoseok@users.noreply.github.com>
2023-08-13 21:59:58 +0900

    [level 1] Title: 삼총사, Time: 0.51 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        Arrays.sort(number);
+        for(int i=0;i<number.length-2;i++){
+            
+            if(number[i]>0){
+                break;
+                
+            }
+            for(int j=i+1;j<number.length-1;j++){
+                
+                for(int k=j+1;k<number.length;k++){
+                    if(number[i]+number[j]+number[k]==0){
+                        answer++;
+                    }
+                }
+                
+            }
+        }
+        return answer;
+    }
+}
