2ee432b5dd8aed05a3876b4593d1f595157d3aeb
IThoseok <122499200+IThoseok@users.noreply.github.com>
2023-09-22 23:59:15 +0900

    [level 1] Title: 체육복, Time: 0.94 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        int count = 0;
+        
+        Set<Integer> set = new TreeSet<>();
+        Set<Integer> set3 = new TreeSet<>();
+        for (int num : reserve) {
+            set.add(num);
+            set3.add(num);
+        }
+        Set<Integer> set2 = new TreeSet<>();
+        for (int num : lost) {
+            set2.add(num);
+        }
+        set.removeAll(set2);
+        set2.removeAll(set3);
+        
+        
+        
+        for (int num : set2) {
+            System.out.println(num);
+            if (set.contains(num)) {
+                set.remove(num);
+            } else if (set.contains(num - 1)) {
+                set.remove(num - 1);
+            } else if (set.contains(num + 1)) {
+                set.remove(num + 1);
+            } else {
+                count++;
+            }
+        }
+        
+        return n-count;
+    }
+}
