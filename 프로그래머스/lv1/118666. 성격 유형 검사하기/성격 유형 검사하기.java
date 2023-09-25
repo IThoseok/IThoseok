5e0fc36d4e427d83cf7c3a140a4b94fd1c7024b4
IThoseok <122499200+IThoseok@users.noreply.github.com>
2023-09-25 18:01:45 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 2.61 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i=0;i<survey.length;i++){
+            if(choices[i]==1||choices[i]==2||choices[i]==3){
+                String type = survey[i].substring(0, 1);
+                map.put(type, map.getOrDefault(type, 0) + Math.abs(choices[i]-4));
+            }else if(choices[i]==5||choices[i]==6||choices[i]==7){
+                String type = survey[i].substring(1);
+                map.put(type, map.getOrDefault(type, 0) + choices[i]-4);
+            }
+        }
+        if(map.getOrDefault("R", 0)>=map.getOrDefault("T", 0)){
+            answer = answer + "R";
+        }else{
+            answer = answer + "T";
+        }
+        
+        if(map.getOrDefault("C", 0)>=map.getOrDefault("F", 0)){
+            answer = answer + "C";
+        }else{
+            answer = answer + "F";
+        }
+        
+        if(map.getOrDefault("J", 0)>=map.getOrDefault("M", 0)){
+            answer = answer + "J";
+        }else{
+            answer = answer + "M";
+        }
+        
+        if(map.getOrDefault("A", 0)>=map.getOrDefault("N", 0)){
+            answer = answer + "A";
+        }else{
+            answer = answer + "N";
+        }
+        
+        
+        
+        return answer;
+    }
+}
