177ff0676842eed8b4f5ae332d1d65764c07b686
IThoseok <122499200+IThoseok@users.noreply.github.com>
2023-09-25 22:18:11 +0900

    [level 1] Title: [1차] 비밀지도, Time: 10.24 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+        for(int i=0;i<arr1.length;i++){
+            String binary = Integer.toBinaryString(arr1[i] | arr2[i]);
+            binary = String.format("%0"+n+"d", Long.parseLong(binary));
+            answer[i] = binary.replaceAll("1", "#").replaceAll("0", " ");
+        }
+        
+        
+        
+        
+        
+        
+        return answer;
+    }
+}
