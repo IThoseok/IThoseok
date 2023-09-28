bbdb7dff72a5b813ae4a7ca5a5d2e8d91b1cb20e
IThoseok <122499200+IThoseok@users.noreply.github.com>
2023-09-28 14:43:37 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.54 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        //Set<String> set1 = new HashSet<>();
+       //Set<String> set2 = new HashSet<>();
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+        
+        str1 =str1.toLowerCase();
+        str2 =str2.toLowerCase();
+        
+        for(int i=0;i<str1.length()-1;i++){
+            char c = str1.charAt(i);
+            char c2 = str1.charAt(i+1);
+            if (Character.isLowerCase(c) && Character.isLowerCase(c2)) {
+                list1.add(String.valueOf(c) + String.valueOf(c2));
+            }
+        }
+        
+        for(int i=0;i<str2.length()-1;i++){
+            char c = str2.charAt(i);
+            char c2 = str2.charAt(i+1);
+            if (Character.isLowerCase(c) && Character.isLowerCase(c2)) {
+                list2.add(String.valueOf(c) + String.valueOf(c2));
+            }
+        }
+        
+        
+        List<String> union = new ArrayList<>(list1);
+        union.addAll(list2);
+        
+        
+        List<String> intersection = new ArrayList<>();
+        for (String str : list1) {
+            if (list2.contains(str)) {
+                intersection.add(str);
+                list2.remove(list2.indexOf(str));
+            }
+        }
+        
+        for (String str : intersection) {
+            if (union.contains(str)) {
+                union.remove(union.indexOf(str));
+            }
+        }
+        
+        System.out.println(union);
+        System.out.println(intersection);
+        
+        if(union.size() == 0) return 65536;
+        
+        return intersection.size() * 65536 / union.size();
+    }
+}
