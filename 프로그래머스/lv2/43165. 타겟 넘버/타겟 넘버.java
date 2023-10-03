201405290c2454c719675b6ef20527180b275483
IThoseok <122499200+IThoseok@users.noreply.github.com>
2023-10-03 23:50:55 +0900

    [level 2] Title: 타겟 넘버, Time: 0.85 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        
+        int[][] Array = new int[numbers.length+1][];
+
+        // 각 행에 배열 할당
+        Array[0] = new int[] {0};
+        for(int i=1;i<=numbers.length;i++){
+            Array[i] = new int[(int)Math.pow(2, i)];
+            for (int j=0; j<Array[i].length; j++){
+                if(j%2==0){ //짝수, 플러스
+                    Array[i][j] = Array[i-1][j/2] + numbers[i-1];
+                }else{//홀수, 마이너스
+                    Array[i][j] = Array[i-1][j/2] - numbers[i-1];
+                }
+                if(i==numbers.length && Array[i][j]==target){
+                    answer++;
+                }
+            }
+        }
+
+        
+        
+        
+        
+        
+        return answer;
+    }
+}
