dcde72a2279595474493b2ecee9b6444b9fa734b
IThoseok <122499200+IThoseok@users.noreply.github.com>
2023-10-06 22:29:42 +0900

    [level 0] Title: 옹알이 (1), Time: 0.15 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,20 +1,6 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        String[] arr = {"aya", "ye", "woo", "ma"};
-        
-        for(String str : babbling){
-            for(int i=0;i<arr.length;i++){
-                if(str.startsWith(arr[i])){
-                    str = str.substring(arr[i].length());
-                    i = -1;
-                }
-            }
-            if(str.isEmpty()){
-                answer++;
-            }
-        }
-        
         return answer;
     }
 }
