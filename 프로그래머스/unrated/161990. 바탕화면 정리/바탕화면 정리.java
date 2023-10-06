6585a956868c6d02ac840b30cb2222e5595d891b
IThoseok <122499200+IThoseok@users.noreply.github.com>
2023-10-06 23:42:00 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.15 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int[] answer = new int[4];
+        answer[0] = 999;
+        answer[1] = 999;
+        answer[2] = -999;
+        answer[3] = -999;
+        
+        for(int i=0;i<wallpaper.length;i++){
+            String str = wallpaper[i];
+            for (int j= 0; j<str.length(); j++) {
+                if('#'==str.charAt(j)){
+                    answer[0] = Math.min(answer[0], i);
+                    answer[1] = Math.min(answer[1], j);
+                    answer[2] = Math.max(answer[2], i+1);
+                    answer[3] = Math.max(answer[3], j+1);
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
